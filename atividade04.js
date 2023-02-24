2abcc61bccbd8e694c998a8a469f29b1e9e92d3f
César Barbosa <93480237+CesarBarbosa01@users.noreply.github.com>
2023-02-24 10:00:16 -0300

    atualizações

--- atividade04.js
+++ atividade04.js
@@ -4,4 +4,10 @@ const ano_nascimento = 2002;
 let date = new Date();
 let ano_atual = date.getFullYear();
 let idade = ano_atual - ano_nascimento;
-console.log(idade);
+console.log(`Idade em anos: ${idade}`);
+let idade_meses = idade * 12;
+console.log(`Idade em meses: ${idade_meses}`)
+let idade_semanas = idade * 52;
+console.log(`idade em semanas: ${idade_semanas}`)
+let idade_dias = idade * 365;
+console.log(`Idade em dias: ${idade_dias}`);
