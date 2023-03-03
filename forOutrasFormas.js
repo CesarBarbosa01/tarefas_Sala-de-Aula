69a8270c927a5b0a1bfb16056e94ab0ad4592ba9
César Barbosa <93480237+CesarBarbosa01@users.noreply.github.com>
2023-03-03 09:57:07 -0300

    novas tarefas

---
+++ forOutrasFormas.js
@@ -0,0 +1,10 @@
+/*
+let cores = ['azul', 'amarelo', 'ciano']
+for (let i in cores) {
+    console.log(cores[i])
+}
+*/
+let cores = ['azul', 'amarelo', 'ciano']
+for (let cor of cores) {
+    console.log(cor)
+}
---
+++ objetovazio.js
@@ -0,0 +1,4 @@
+const obj = {};
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+console.log(Object.entries(obj));
