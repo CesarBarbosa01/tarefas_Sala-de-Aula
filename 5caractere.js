a740c4581842f0783e30d2319cefe4558f13ffb8
César Barbosa <edinaldocesar14@gmail.com>
2023-03-11 23:22:31 -0300

    aprendizado1

--- 5caractere.js
+++ 5caractere.js
@@ -1,3 +1,4 @@
+/*
 function filtrarStrings(arrayDeStrings) {
     const novaArray = arrayDeStrings.filter((string) => string.length > 5);
     return novaArray;
@@ -6,4 +7,10 @@ function filtrarStrings(arrayDeStrings) {
   const arrayDeStrings = ["foo", "bar", "foobar", "baz", "qux", "quux"];
   const novaArray = filtrarStrings(arrayDeStrings);
   console.log(novaArray); 
- 
+
+  const RandomNumbers = [23, 24, 10, 54, 100]
+  const MaiorQue37 = RandomNumbers.filter(maior => maior > 37 )
+  console.log(MaiorQue37) 
+ */
+
+  
